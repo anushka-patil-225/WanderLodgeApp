ba79e70cbe11a6503d66967a9fa63b6f78948483
anushka-patil-225 <anushkapatil566@gmail.com>
2024-10-26 19:35:43 +0530

    Images url checking

--- WanderLodge/client/src/Image.jsx
+++ WanderLodge/client/src/Image.jsx
@@ -1,4 +1,4 @@
 export default function Image({ src, ...rest }) {
-  src = src && src.includes("https://") ? src : "https://localhost:4000/" + src;
+  src = src && src.includes("https://") ? src : "http://localhost:4000/" + src;
   return <img {...rest} src={src} alt={""} />;
 }
