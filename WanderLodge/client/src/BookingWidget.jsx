7e40e8f93ce7292e9a5aca0ec9bfc33b6e0dc822
anushka-patil-225 <anushkapatil566@gmail.com>
2024-10-26 11:03:35 +0530

    testing with booking url

--- WanderLodge/client/src/BookingWidget.jsx
+++ WanderLodge/client/src/BookingWidget.jsx
@@ -1,17 +1,17 @@
-import {useContext, useEffect, useState } from "react";
-import {differenceInCalendarDays} from "date-fns";
-import { Navigate} from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { differenceInCalendarDays } from "date-fns";
+import { Navigate } from "react-router-dom";
 import axios from "axios";
-import {UserContext} from "./UserContext.jsx";
+import { UserContext } from "./UserContext.jsx";
 
-export default function({place}){
-  const [checkIn,setCheckIn] = useState('');
-  const [checkOut,setCheckOut] = useState('');
-  const [numberOfGuests,setNumberOfGuests] = useState(1);
-  const [name,setName] = useState('');
-  const [phone,setPhone] = useState('');
-  const [redirect,setRedirect] = useState('');
-  const {user} = useContext(UserContext);
+export default function ({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [redirect, setRedirect] = useState("");
+  const { user } = useContext(UserContext);
 
   useEffect(() => {
     if (user) {
@@ -21,69 +21,86 @@ export default function({place}){
 
   let numberOfNights = 0;
   if (checkIn && checkOut) {
-    numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
+    numberOfNights = differenceInCalendarDays(
+      new Date(checkOut),
+      new Date(checkIn)
+    );
   }
 
   async function bookThisPlace() {
-    const response = await axios.post('/bookings', {
-      checkIn,checkOut,numberOfGuests,name,phone,
-      place:place._id,
-      price:numberOfNights * place.price,
-    });
+    const response = await axios.post(
+      "https://wanderlodgeapp.onrender.com/bookings",
+      {
+        checkIn,
+        checkOut,
+        numberOfGuests,
+        name,
+        phone,
+        place: place._id,
+        price: numberOfNights * place.price,
+      }
+    );
     const bookingId = response.data._id;
     setRedirect(`/account/bookings/${bookingId}`);
   }
 
   if (redirect) {
-    return <Navigate to={redirect} />
+    return <Navigate to={redirect} />;
   }
 
-    return(
-        <div className="bg-white shadow p-4 rounded-2xl">
-        <div className="text-2xl text-center">
+  return (
+    <div className="bg-white shadow p-4 rounded-2xl">
+      <div className="text-2xl text-center">
         Price: Rs.{place.price}/per night
-        </div>
-        <div className="border rounded-2xl mt-4">   
-          <div className="flex">
+      </div>
+      <div className="border rounded-2xl mt-4">
+        <div className="flex">
           <div className="py-3 px-4">
-           <label>Check-in:</label>
-           <input type="date" 
-                  value={checkIn}
-                  onChange={ev => setCheckIn(ev.target.value)}/>
+            <label>Check-in:</label>
+            <input
+              type="date"
+              value={checkIn}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+            />
+          </div>
+          <div className="py-3 px-4 border-l">
+            <label>Check-out:</label>
+            <input
+              type="date"
+              value={checkOut}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+            />
+          </div>
         </div>
         <div className="py-3 px-4 border-l">
-           <label>Check-out:</label>
-           <input type="date" 
-                  value={checkOut}
-                  onChange={ev => setCheckOut(ev.target.value)}/>
-        </div>
-          </div>   
-          <div className="py-3 px-4 border-l">
           <label>Number of Guests:</label>
-          <input type="number"
-                 value={numberOfGuests}
-                 onChange={ev => setNumberOfGuests(ev.target.value)}/>
-          </div> 
-          {numberOfNights > 0 && (
+          <input
+            type="number"
+            value={numberOfGuests}
+            onChange={(ev) => setNumberOfGuests(ev.target.value)}
+          />
+        </div>
+        {numberOfNights > 0 && (
           <div className="py-3 px-4 border-t">
             <label>Your full name:</label>
-            <input type="text"
-                   value={name}
-                   onChange={ev => setName(ev.target.value)}/>
+            <input
+              type="text"
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+            />
             <label>Phone number:</label>
-            <input type="tel"
-                   value={phone}
-                   onChange={ev => setPhone(ev.target.value)}/>
-          </div> 
-          )}     
+            <input
+              type="tel"
+              value={phone}
+              onChange={(ev) => setPhone(ev.target.value)}
+            />
           </div>
-        <button onClick={bookThisPlace} className="primary mt-4">
-          Book this place
-          {numberOfNights &&(
-           <span> Rs.{numberOfNights * place.price}
-          </span>
-          )}
-          </button>
+        )}
       </div>
-    );
+      <button onClick={bookThisPlace} className="primary mt-4">
+        Book this place
+        {numberOfNights && <span> Rs.{numberOfNights * place.price}</span>}
+      </button>
+    </div>
+  );
 }
