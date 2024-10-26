1e01be8dec155c07f0f8c69a85ba643d02798fcc
anushka-patil-225 <anushkapatil566@gmail.com>
2024-10-26 10:30:27 +0530

    corrections in images url backend

--- WanderLodge/client/src/Image.jsx
+++ WanderLodge/client/src/Image.jsx
@@ -2,6 +2,6 @@ export default function Image({ src, ...rest }) {
   src =
     src && src.includes("https://")
       ? src
-      : "https://wanderlodgeapp.onrender.com" + src;
+      : "https://wanderlodgeapp.onrender.com/" + src;
   return <img {...rest} src={src} alt={""} />;
 }
