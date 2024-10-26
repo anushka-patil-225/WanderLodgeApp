f50a205d908cd0d578c14fa72f2bc9ff282c0439
anushka-patil-225 <anushkapatil566@gmail.com>
2024-10-26 11:19:33 +0530

    Adding urls

--- WanderLodge/api/index.js
+++ WanderLodge/api/index.js
@@ -24,10 +24,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "https://wander-lodge-app.vercel.app",
-      "https://wanderlodgeapp.onrender.com",
-    ],
+    origin: ["https://wander-lodge-app.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
