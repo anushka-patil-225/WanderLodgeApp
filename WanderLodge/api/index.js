c07705c1da81311a3d3309fad94899616cdd6770
anushka-patil-225 <anushkapatil566@gmail.com>
2024-10-26 09:41:08 +0530

    Cors resolved for all

--- WanderLodge/api/index.js
+++ WanderLodge/api/index.js
@@ -24,10 +24,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "https://wander-lodge-app.vercel.app",
-      "https://wanderlodgeapp.onrender.com",
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
